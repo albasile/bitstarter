8b88c66eb3f1e3c931716f932437f2af8adb3f77
Antonio Luiz Basile <albasile2@gmail.com>
2013-07-09 21:10:01 +0000

    atualizado web.js

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
+var fs = require('fs');
+
+var buffer = fs.readFileSync('index.html');
+
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
